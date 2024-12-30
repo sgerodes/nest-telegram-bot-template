e06db1831850379a609518e52848175f53ad38c8
sgerodes <sgerodes@gmail.com>
2024-12-30 12:14:45 +0100

    feat: lint and format prisma

--- prisma/createSchemas.ts
+++ prisma/createSchemas.ts
@@ -7,15 +7,17 @@ const postgresTargetSchema = path.join(__dirname, './schema.postgresql.prisma');
 
 let schema = fs.readFileSync(sourceSchema, 'utf-8');
 
-const dataSourceSqlite = "datasource db {\n" +
-  "  provider = \"sqlite\"\n" +
-  "  url      = env(\"DATABASE_URL\")\n" +
-  "}\n\n";
+const dataSourceSqlite =
+  'datasource db {\n' +
+  '  provider = "sqlite"\n' +
+  '  url      = env("DATABASE_URL")\n' +
+  '}\n\n';
 
-const dataSourcePostgres = "datasource db {\n" +
-  "  provider = \"postgresql\"\n" +
-  "  url      = env(\"DATABASE_URL\")\n" +
-  "}\n\n";
+const dataSourcePostgres =
+  'datasource db {\n' +
+  '  provider = "postgresql"\n' +
+  '  url      = env("DATABASE_URL")\n' +
+  '}\n\n';
 
 fs.writeFileSync(sqliteTargetSchema, dataSourceSqlite + schema, 'utf-8');
 console.log(`Sqlite schema generated into ${sqliteTargetSchema}.`);

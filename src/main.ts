a81876ebc6dfe0bf9df104780a69e7f3164559be
sgerodes <sgerodes@gmail.com>
2025-01-30 22:32:34 +0100

    feat: minor changes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
-import { RootConfig } from '@configuration/configuration.models';
-
-const logger = new Logger('main');
 
 async function bootstrap() {
   const _app = await NestFactory.createApplicationContext(AppModule);

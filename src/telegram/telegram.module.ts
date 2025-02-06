8cf1be9005d0c14d6eb3530d390af4e37ea9e3eb
sgerodes <sgerodes@gmail.com>
2025-02-06 19:41:58 +0100

    feat: update i18n setup

--- src/telegram/telegram.module.ts
+++ src/telegram/telegram.module.ts
@@ -6,32 +6,35 @@ import { DatabaseModule } from '@database/database.module';
 import { UserRepositoryService } from '@database/user-repository/user-repository.service';
 import { LanguageModule } from '@language/language.module';
 import {
+  TelegrafI18nModule,
+  TelegrafI18nMiddlewareProvider,
   TelegrafI18nContext,
-  TelegrafI18nMiddleware,
 } from 'nestjs-telegraf-i18n';
 import { session } from 'telegraf';
 import { SceneHello } from '../scenes/hello.scene';
 
 @Module({
   imports: [
+    TelegrafI18nModule,
     TelegrafModule.forRootAsync({
-      inject: [TelegramConfig],
-      useFactory: (telegramConfig: TelegramConfig) => ({
+      inject: [TelegramConfig, TelegrafI18nMiddlewareProvider],
+      useFactory: (
+        telegramConfig: TelegramConfig,
+        telegrafI18nMiddlewareProvider: TelegrafI18nMiddlewareProvider,
+      ) => ({
         token: telegramConfig.bot.token,
         options: {
           contextType: TelegrafI18nContext,
         },
-        middlewares: [session()],
+        middlewares: [
+          session(),
+          telegrafI18nMiddlewareProvider.telegrafI18nMiddleware,
+        ],
       }),
     }),
     DatabaseModule,
     LanguageModule,
   ],
-  providers: [
-    BotUpdate,
-    UserRepositoryService,
-    TelegrafI18nMiddleware,
-    SceneHello,
-  ],
+  providers: [UserRepositoryService, BotUpdate, SceneHello],
 })
 export class TelegramModule {}

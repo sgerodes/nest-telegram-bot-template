b3c9799bd1a9f246802202d7b4fc275244ca5be0
sgerodes <sgerodes@gmail.com>
2025-02-24 11:08:18 +0100

    feat: remove next from wizzards

--- src/telegram/scenes/hello.scene.ts
+++ src/telegram/scenes/hello.scene.ts
@@ -1,6 +1,6 @@
 import { SCENES } from '@configuration/telegramConstants';
 import { i18nKeys } from '@i18n/i18n.keys';
-import {Ctx, SceneEnter, Wizard, WizardStep} from 'nestjs-telegraf';
+import { Ctx, SceneEnter, Wizard, WizardStep } from 'nestjs-telegraf';
 import { Logger } from '@nestjs/common';
 import { WizardI18nContext } from '@telegram/utils';
 
@@ -8,15 +8,13 @@ import { WizardI18nContext } from '@telegram/utils';
 export class SceneHello {
   private readonly logger = new Logger(this.constructor.name);
 
-  // @WizardStep(1)
   @SceneEnter()
-  async step1(@Ctx() ctx: WizardI18nContext) {
+  async onEnter(@Ctx() ctx: WizardI18nContext) {
     await ctx.reply(ctx.t(i18nKeys.i18n.command.hello.scene.first_message));
-    ctx.wizard.next();
   }
 
   @WizardStep(1)
-  async step2(@Ctx() ctx: WizardI18nContext) {
+  async step1(@Ctx() ctx: WizardI18nContext) {
     await ctx.reply(ctx.t(i18nKeys.i18n.command.hello.scene.second_message));
     await ctx.scene.leave();
   }

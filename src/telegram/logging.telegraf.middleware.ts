5a9e69e2afe8920278eabff40fa975fea754bcec
sgerodes <sgerodes@gmail.com>
2025-02-24 10:55:33 +0100

    feat: add better logging

--- src/telegram/logging.telegraf.middleware.ts
+++ src/telegram/logging.telegraf.middleware.ts
@@ -10,6 +10,6 @@ export const loggingMiddleware: Middleware<TelegrafI18nContext> = async (
   ctx: TelegrafI18nContext,
   next,
 ) => {
-  logger.debug(`Received message from ${ctx.from.id}`);
+  logger.debug(`Received message from ${ctx.from.id}, first_name=${ctx?.from?.first_name}, last_name=${ctx?.from?.last_name}, username=${ctx?.from?.username}`);
   await next();
 };
--- src/telegram/scenes/hello.scene.ts
+++ src/telegram/scenes/hello.scene.ts
@@ -1,6 +1,6 @@
 import { SCENES } from '@configuration/telegramConstants';
 import { i18nKeys } from '@i18n/i18n.keys';
-import { Ctx, Wizard, WizardStep } from 'nestjs-telegraf';
+import {Ctx, SceneEnter, Wizard, WizardStep} from 'nestjs-telegraf';
 import { Logger } from '@nestjs/common';
 import { WizardI18nContext } from '@telegram/utils';
 
@@ -8,13 +8,14 @@ import { WizardI18nContext } from '@telegram/utils';
 export class SceneHello {
   private readonly logger = new Logger(this.constructor.name);
 
-  @WizardStep(1)
+  // @WizardStep(1)
+  @SceneEnter()
   async step1(@Ctx() ctx: WizardI18nContext) {
     await ctx.reply(ctx.t(i18nKeys.i18n.command.hello.scene.first_message));
     ctx.wizard.next();
   }
 
-  @WizardStep(2)
+  @WizardStep(1)
   async step2(@Ctx() ctx: WizardI18nContext) {
     await ctx.reply(ctx.t(i18nKeys.i18n.command.hello.scene.second_message));
     await ctx.scene.leave();

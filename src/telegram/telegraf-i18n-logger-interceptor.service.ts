d75084c4a48b89f86216ea50364bb4cbe75788da
sgerodes <sgerodes@gmail.com>
2025-02-28 15:59:12 +0100

    feat: add logger interceptors

--- src/telegram/telegraf-i18n-logger-interceptor.service.ts
+++ src/telegram/telegraf-i18n-logger-interceptor.service.ts
@@ -1,19 +1,18 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 
 @Injectable()
-export class I18nLoggerInterceptor implements OnModuleInit {
+export class TelegrafI18nLoggerInterceptor implements OnModuleInit {
     private readonly logger = new Logger(this.constructor.name);
     private static readonly suppressed_messages: string = 'context type: telegraf not supported';
 
     onModuleInit() {
-        this.logger.debug(`Intercepting I18nService Logger to suppress unwanted warnings: '${I18nLoggerInterceptor.suppressed_messages}'`);
+        this.logger.debug(`Intercepting I18nService Logger to suppress unwanted warnings: '${TelegrafI18nLoggerInterceptor.suppressed_messages}'`);
         const lo = this.logger;
 
-        // Override NestJS Logger globally
         const originalWarn = Logger.prototype.warn;
         Logger.prototype.warn = function (message: any, ...optionalParams: any[]) {
-            lo.log(`interception Warn: '${message}'`);
-            if (typeof message === 'string' && message.includes(I18nLoggerInterceptor.suppressed_messages)) {
+            lo.log(`Warn Intercepted: '${message}'`);
+            if (typeof message === 'string' && message.startsWith(TelegrafI18nLoggerInterceptor.suppressed_messages)) {
                 return; // Suppress this specific warning
             }
             originalWarn.call(this, message, ...optionalParams);
